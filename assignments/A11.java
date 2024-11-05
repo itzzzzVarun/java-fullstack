053ce3618aad9182e76377e7ea06f0c7bd62ee67
Varun Singh <iamvarunking619@gmail.com>
2024-11-05 14:34:10 +0530

    completed some more assignments in core-java

---
+++ assignments/A11.java
@@ -0,0 +1,14 @@
+//non-static variables
+
+public class A11 {
+  int a = 12;
+  double b = 34.7;
+  String c = "Deepty";
+
+  public static void main(String[] args) {
+    A11 a1 = new A11();
+    System.out.println(a1.a);
+    System.out.println(a1.b);
+    System.out.println(a1.c);
+  }
+}

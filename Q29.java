36141438e66f6a53f2daaab29cae1aeb29895985
Varun Singh <iamvarunking619@gmail.com>
2024-11-19 20:59:07 +0530

    solved some more questions

---
+++ Q29.java
@@ -0,0 +1,11 @@
+//for loop: for loop is entry control loop which check condition first and then loop body executes. 
+//for (initialization; condition; increment/decrement) {  body  }
+
+//break keyword: break is a keyword which is used inside loop or switch case to terminate the execution of loop or switch block. When JVM reads break keyword then immidiately JVM will come out of the block of loop or switch.
+
+//continue keyword: continue is a keyword which is used to skip one or more exxcutions from the loop. When JVM reads continue keyword then immidiately it is transfered to increament or decreament of the loop. 
+public class Q29 {
+  public static void main(String[] args) {
+
+  }
+}
